8131f375e0e7e0e6c900bee37d21c401078cc30b
Alexander Verner <Verner.Alx@gmail.com>
2021-09-09 23:50:34 +0500

    Fix more dishes and add sum

--- project/views.py
+++ project/views.py
@@ -7,21 +7,33 @@ def first_page(request):
 
 def checkout(request):
     checks = request.POST.getlist('checks')
-    print(checks)  # Проверяем наличие id галочек
     array_num = [int(item) for item in checks]  # перевод в числовой вид из стрингового представления
     appen = []
-    flag = False # флаг для определения 2 и более выбранных элементов (для if-else в HTML-шаблоне)
-    if (len(array_num)>1):
+    flag = False
+    summ = 0
+    if (len(array_num) != 0):
         for el in range(0, len(array_num)):
-            flag = True
             count = array_num[el]  # получение значения первого элемента листа
             chk = MenuShow.objects.get(id=count)  # выбираем из БД значение, равное id объекта в БД
-            print(chk)
             appen.append(chk)  # присоединение к массиву для вывода двух и более галочек в итоговой таблице
+            summ += int(chk.menu_price)
+            chk_id = chk.id
+            menu_title = chk.menu_title
+            menu_allergens = chk.menu_allergens
+            menu_price = chk.menu_price
         checks_list = appen
+        flag = True
     else:
-        checks_list = MenuShow.objects.get(id=array_num[0])
         flag = False
-        print(checks_list)
-    print(appen)
-    return render(request, './checkout.html', {'checks': checks, 'checks_list': checks_list, 'flag': flag})
+        checks_list = []
+        menu_title = ''
+        menu_allergens = ''
+        menu_price = ''
+        summ = 0
+    return render(request, './checkout.html', {'checks': checks,
+                                               'checks_list': checks_list,
+                                               'summ' : summ,
+                                               'menu_title': menu_title,
+                                               'menu_allergens' : menu_allergens,
+                                               'menu_price' : menu_price,
+                                               'flag' : flag,})
