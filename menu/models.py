077083fec3497f59271d5f5ad176f2de32a42324
Alexander Verner <Verner.Alx@gmail.com>
2021-09-10 01:12:18 +0500

    Change Field in models.py

--- menu/models.py
+++ menu/models.py
@@ -3,13 +3,23 @@ from django.db import models
 # Create your models here.
 
 class MenuShow(models.Model):
+    CATEGORY_CHOICES = [
+        ('salads', 'Салат'),
+        ('soup', 'Суп'),
+        ('hotter', 'Горячее'),
+        ('grill', 'Гриль'),
+    ]
     menu_img = models.ImageField(upload_to='imgmenu/', verbose_name='Фото блюда')
     menu_title = models.CharField(max_length=100, verbose_name='Название')
-    menu_ingredients = models.CharField(max_length=300, verbose_name='Состав')
-    menu_price = models.CharField(max_length=100, verbose_name='Цена, руб.')
-    menu_category = models.CharField(max_length=100, verbose_name= 'Категория (salads, soup, hotter, grill)')
+    menu_ingredients = models.JSONField(verbose_name='Состав')
+    menu_price = models.IntegerField(verbose_name='Цена, руб.')
+    menu_category = models.CharField(
+        max_length=20,
+        choices=CATEGORY_CHOICES,
+        verbose_name='Категория'
+    )
     menu_allergens = models.CharField(max_length=300, null=True, default='Нет', verbose_name='Наличие аллергенов')
-    menu_kkal = models.CharField(max_length=100, null=True, default='0', verbose_name='Пищевая ценность Ккал')
+    menu_kkal = models.IntegerField(null=True, default=0, verbose_name='Пищевая ценность Ккал')
 
     def __str__(self):
         return self.menu_title
